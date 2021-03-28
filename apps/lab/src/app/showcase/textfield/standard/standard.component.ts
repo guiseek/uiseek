e55b80429020cd6f42f3f9f2c295b0d90a3ee44a
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-03-28 14:54:49 -0300

    feat(uiseek): helper & output text

--- apps/lab/src/app/showcase/textfield/standard/standard.component.ts
+++ apps/lab/src/app/showcase/textfield/standard/standard.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoelCaseTextFieldBase } from '../textfield-base';
 
 @Component({
   selector: 'uiseek-standard',
   templateUrl: './standard.component.html',
   styleUrls: ['./standard.component.scss'],
 })
-export class StandardComponent implements OnInit {
-  constructor() {}
+export class StandardComponent extends ShoelCaseTextFieldBase implements OnInit {
+  // constructor() {}
 
   ngOnInit(): void {}
 }
---
+++ libs/uiseek/src/lib/textfields/common/helper/helper.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'label[uiseek]>small[uiseek]',
+  template: '<ng-content></ng-content>',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HelperComponent {}
